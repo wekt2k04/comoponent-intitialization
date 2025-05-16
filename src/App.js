39c8a96131f465d969b1dd5bdbc2f55a761a1357
wekt2k04 <k.tsetse0026@uca.ac.ma>
2025-05-16 08:15:29 +0100

    adding a clickHandler function inside the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import './ModeToogler.css';
-import ModeToogler from './ModeToogler'
-
 function App() {
+  function handleClicker() {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    let userInput = prompt("Veillez entrer votre numéro: ")
+    alert(`Computer number: ${randomNum}, Your Guess: ${userInput}`)
+  }
   return (
     <>
-    <ModeToogler />
+    <h1>Welcome to the "Guess Me" GAME !</h1>
+    <button onClick={handleClicker}>Guess a number between 1 and 3</button>
     </>
   );
 }
