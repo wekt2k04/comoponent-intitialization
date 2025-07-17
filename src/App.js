32a8ac35097cdce4570fd6c099cf114ebfb7258a
wekt2k04 <k.tsetse0026@uca.ac.ma>
2025-07-17 09:37:41 +0100

    Small Meal App : Managing state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import TextInputWithFocusButton from './TextInputWithFocusButton';
+import MealsProvider from './providers/MealsProvider'; 
+import MealsList from './components/MealsList'; 
+import Counter from './components/Counter'; 
 import './App.css'; 
-
 
 function App() {
   return (
-    <TextInputWithFocusButton />
+    <div>
+      <MealsProvider >
+      <MealsList />
+      <Counter />
+      </MealsProvider>
+    </div>
   );
 }
 
